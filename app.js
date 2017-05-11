d06140ef3c006264519a1a1046015f8dcea41517
Adam Butler <adam@lab.io>
2017-05-11 12:37:15 +0100

    Add coupon support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import env from 'node-env-file'
 import apiai from 'apiai'
 import Nexmo from 'nexmo'
 import Pusher from 'pusher'
+import redis from 'redis'
 
 // Load the env file
 try {
@@ -11,6 +12,8 @@ try {
 } catch (error) {
   console.log(error)
 }
+
+const client = redis.createClient()
 
 const app = express()
 const bot = apiai(process.env.API_AI_CLIENT_TOKEN)
@@ -38,7 +41,7 @@ const pushMessage = function (message, number) {
   })
 }
 
-const tryGreetingWithCnam = function (number) {
+const tryGreetingWithCnam = function (number, fallback) {
   nexmo.numberInsight.get({level: 'standard', number: number, cnam: true}, function (error, payload) {
     if (error) {
       console.log('Error with NI')
@@ -46,7 +49,7 @@ const tryGreetingWithCnam = function (number) {
       if(payload && payload.first_name && payload.last_name) {
         pushMessage(`Hello ${payload.first_name} ${payload.last_name.slice(0, 1)}!`, number)
       } else {
-        pushMessage(`Hello!`, number)
+        pushMessage(fallback, number)
       }
     }
   })
@@ -66,7 +69,7 @@ const performBotAction = function(input, number) {
     switch (response.result.action) {
       case 'smalltalk.greetings.hello':
         if (process.env.CNAM_ENABLED && process.env.CNAM_ENABLED === 'true') {
-          tryGreetingWithCnam(number)
+          tryGreetingWithCnam(number, response.result.fulfillment.speech)
         } else {
           pushMessage(response.result.fulfillment.speech, number)
         }
@@ -83,13 +86,60 @@ const performBotAction = function(input, number) {
   botRequest.end()
 }
 
+const issueCoupon = function(inboundNumberFrom, inboundNumberTo, state = {}) {
+  const text = process.env.COUPON_TEXT.replace('{COUPON}', process.env.COUPON)
+
+  const outboundNumberFrom = inboundNumberTo
+  const outboundNumberTo = inboundNumberFrom
+
+  nexmo.message.sendSms(outboundNumberFrom, outboundNumberTo, text, function(err, response) {
+    if (err) {
+      console.log("SMS could not be sent!");
+      console.log(err);
+    } else {
+      if (response.messages[0].status == '0') {
+        const payload = {
+          ...state,
+          coupon_issued: true
+        }
+
+        console.log(payload);
+
+        client.hset('numbers', outboundNumberTo, JSON.stringify(payload))
+      } else {
+        console.log('Non 0 status from Nexmo');
+        console.log(response);
+      }
+    }
+  });
+}
+
+const tryIssueCoupon = function(inboundNumberFrom, inboundNumberTo) {
+  if(process.env.COUPON && process.env.COUPON_TEXT) {
+    client.hget('numbers', inboundNumberFrom, function(err, result) {
+      if (result) {
+        const payload = JSON.parse(result)
+        if(!payload.coupon_issued) {
+          issueCoupon(inboundNumberFrom, inboundNumberTo, payload)
+        } else {
+          console.log(`Coupon already issued for ${inboundNumberFrom}`);
+        }
+      } else {
+        issueCoupon(inboundNumberFrom, inboundNumberTo)
+      }
+    })
+  }
+}
+
 app.post('/sms', (req, res) => {
   performBotAction(req.body.text, req.body.msisdn)
+  tryIssueCoupon(req.body.msisdn, req.body.to)
   res.sendStatus(200)
 })
 
 app.get('/sms', (req, res) => {
   performBotAction(req.query.text, req.query.msisdn)
+  tryIssueCoupon(req.query.msisdn, req.query.to)
   res.sendStatus(200)
 })
 
